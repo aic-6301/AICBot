6b02ddf64daf03b4c5741093fdaa4162e2d9718c
aic-6301 <rx435423@gmail.com>
2024-01-24 22:25:37 +0900

    Spotifyの再生中の曲を送信 Fixes #7

--- cogs/spotify.py
+++ cogs/spotify.py
@@ -33,7 +33,7 @@ class spotify(commands.Cog):
                                 # embed = discord.Embed(title=f"{after.display_name}がSpotifyで{activities.title}を再生中です!", description=f"再生中 - {activities.title}\n 残り{discord.utils.format_dt(activities.end, style='R')}", color=discord.Color.from_str(0x1DB954), url=activities.track_url)
                                 embed.add_field(name="タイトル", value=activities.title, inline=True)
                                 embed.add_field(name="アルバム", value=f"{activities.album}")
-                                embed.add_field(name="歌っている人", value=f"{activities.artist}")
+                                embed.add_field(name="作者", value=f"{activities.artist}")
                                 view = discord.ui.View()
                                 view.add_item(discord.ui.Button(label=f"{activities.artist} 検索", style=discord.ButtonStyle.url, url=f"https://google.com/search?q={(activities.artist)}"))
                                 view.add_item(discord.ui.Button(label=f"聴いてみる！", style=discord.ButtonStyle.url, url=activities.track_url))
@@ -57,14 +57,14 @@ class spotify(commands.Cog):
     @app_commands.command(name="spotify", description="再生中の曲を表示します")
     async def spotify(self, interaction: discord.Interaction):
         print("fire")
-        print(interaction.user)
+        print(interaction.user.activities)
         for activities in interaction.user.activities:
             if isinstance(activities, discord.Spotify):
                 embed = discord.Embed(title=f"{interaction.user.display_name}の再生中の曲", description=f"再生中 - {activities.title}\n 残り{discord.utils.format_dt(activities.end, style='R')}",color=discord.Color.from_str("#0x1DB954"), url=activities.track_url)
                 # embed = discord.Embed(title=f"{after.display_name}がSpotifyで{activities.title}を再生中です!", description=f"再生中 - {activities.title}\n 残り{discord.utils.format_dt(activities.end, style='R')}", color=discord.Color.from_str(0x1DB954), url=activities.track_url)
                 embed.add_field(name="タイトル", value=activities.title, inline=True)
                 embed.add_field(name="アルバム", value=f"{activities.album}")
-                embed.add_field(name="歌っている人", value=f"{activities.artist}")
+                embed.add_field(name="作者", value=f"{activities.artist}")
                 view = discord.ui.View()
                 view.add_item(discord.ui.Button(label=f"{activities.artist} 検索", style=discord.ButtonStyle.url, url=f"https://google.com/search?q={(activities.artist)}"))
                 view.add_item(discord.ui.Button(label=f"聴いてみる！", style=discord.ButtonStyle.url, url=activities.track_url))
@@ -74,8 +74,12 @@ class spotify(commands.Cog):
                 else:
                     pass
                 await interaction.response.send_message(embed=embed, view=view)
+                return
             else:
                 await interaction.response.send_message(embed=discord.Embed(title="エラー", description="何も再生していません！", color=discord.Color.red()))
+                return
+        await interaction.response.send_message(embed=discord.Embed(title="エラー", description="何も再生していません！", color=discord.Color.red()))
+        return
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(spotify(bot))
